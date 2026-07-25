091794997c5beb52cdd9afaaee8f4d8caa2192e3
zulkifli09791 <zulkiflikifli91219@gmail.com>
2026-07-26 00:58:54 +0700

    Fix Android namespace for legacy plugins like isar

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,26 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    // Fix for legacy plugins (like isar_flutter_libs) that don't specify a namespace
+    afterEvaluate {
+        val androidExtension = extensions.findByName("android")
+        if (androidExtension != null) {
+            val namespaceProperty = androidExtension::class.java.methods.find { it.name == "getNamespace" }
+            val setNamespaceMethod = androidExtension::class.java.methods.find { it.name == "setNamespace" }
+            if (namespaceProperty != null && setNamespaceMethod != null) {
+                val currentNamespace = namespaceProperty.invoke(androidExtension)
+                if (currentNamespace == null) {
+                    val groupName = project.group.toString()
+                    if (groupName.isNotEmpty()) {
+                        setNamespaceMethod.invoke(androidExtension, groupName)
+                    } else {
+                        setNamespaceMethod.invoke(androidExtension, "com.example.${project.name.replace("-", "_")}")
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
